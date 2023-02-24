5616c7d8c677a4cffa13507c5d54a187a68131f9
Jagoba Ariztegi <jagobaariztegi@gmail.com>
2023-02-24 15:42:39 +0100

    remove root config and awesomewm config

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@ sudo pacman -S --needed base-devel
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
-
 cd ..
 rm -Rf paru
-sudo pacman -Syu sddm bat exa mdcat brightnessctl nemo fzf pacman-contrib man-db lazygit
+pacman -Sy --needed - <pacman.lst
+paru -Sy --needed - <paru.lst
 git clone https://github.com/neovim/neovim
 cd neovim && make CMAKE_BUILD_TYPE=Release
 sudo make install
@@ -17,17 +17,16 @@ mkdir ~/.npm-global
 npm config set prefix '~/.npm-global'
 
 bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
-paru -S rofi-lbonn-wayland-git sddm-sugar-dark hyprpaper-git grim slurp sioyek hyprland-nvidia
-cp -r user/.config/rofi ~/.config/
-cp -r user/.config/dunst ~/.config/
-cp -r user/.config/hypr ~/.config/
-cp -r user/.config/waybar ~/.config/
-cp -r user/.config/kitty ~/.config/
+cp -r .config/rofi ~/.config/
+cp -r .config/dunst ~/.config/
+cp -r .config/hypr ~/.config/
+cp -r .config/waybar ~/.config/
+cp -r .config/kitty ~/.config/
 
 mkdir -p ~/.local/share/fonts
-cp -r user/.local/fonts/* ~/.local/share/fonts
+cp -r .local/fonts/* ~/.local/share/fonts
 mkdir -p ~/.local/bin
-cp -r user/.local/bin/* ~/.local/bin
-cp user/.zshrc ~/
+cp -r .local/bin/* ~/.local/bin
+cp .zshrc ~/
 
 zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) -y
--- user/.config/awesome.my/scripts/ranger.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-kitty -e sh -c "sleep 0.02 && ranger"
