0132fe481285050936620011d5d54911fbd9ee2d
Jagoba Ariztegi <jagobaariztegi@gmail.com>
2023-03-01 17:25:20 +0100

    feat(packages): enable parallel download

---
+++ .install/create-folder.sh
@@ -0,0 +1,4 @@
+# Create default folder structure
+xdg-user-dirs-update
+
+echo ":: Default folder structure created"
--- install.sh
+++ install.sh
@@ -3,12 +3,14 @@
 
 source .install/includes/colors.sh
 source .install/includes/library.sh
+source .install/packages/paralleldownloads.sh
 source .install/packages/packages.sh
 source .install/packages/required.sh
 source .install/packages/aur.sh
 source .install/backup.sh
 source .install/packages/install-packages.sh
 source .install/packages/activate-services.sh
+source .install/create-folder.sh
 source .install/keyboard.sh
 source .install/displaymanager.sh
 source .install/neovim.sh
